c5cb4ca3d27fa813ed46b787313327f5365ff1bc
BurnerAce <hanzalasharique04@gmail.com>
2025-02-08 18:29:14 +0530

    Footer fixed

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "../css/footer.css";
 
 const Footer = () => {
-  return <div className="footer-box"></div>;
+  return (
+    <footer className="footer">
+      <h2>Enterprise Management System</h2>
+      {/* <p>
+        Mail: <a href="mailto:exxpensify.pro@company.com">exxpensify.pro@company.com</a>
+      </p>
+      <p className="terms">Terms and Conditions apply <span>*</span></p> */}
+    </footer>
+  );
 };
 
 export default Footer;
